15a168b06b8e3dc89137809b3c796a11ab4ef60c
Deac <deac.karns@gmail.com>
2016-05-02 19:54:28 -0400

    bumping version

--- messagemus-prime.rb
+++ messagemus-prime.rb
@@ -1,7 +1,7 @@
 class MessagemusPrime < Formula
   desc "CLI utility for displaying messages to the top of the terminal"
   homepage "https://github.com/peledies/messagemus-prime"
-  url "https://github.com/peledies/messagemus-prime/archive/0.0.2.tar.gz"
+  url "https://github.com/peledies/messagemus-prime/archive/0.0.3.tar.gz"
   sha256 "2c34f4cbe78ea1df53817a5fcc805e82668eeed084ddb2aaa1ed2c50b72cc8ca"
 
   def install
