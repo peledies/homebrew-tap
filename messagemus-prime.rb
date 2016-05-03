fa5c64c3c5954c7edb2d7bbc7861460dc33f71ce
Deac <deac.karns@gmail.com>
2016-05-02 22:33:54 -0400

    updating checksum

--- messagemus-prime.rb
+++ messagemus-prime.rb
@@ -2,7 +2,7 @@ class MessagemusPrime < Formula
   desc "CLI utility for displaying messages to the top of the terminal"
   homepage "https://github.com/peledies/messagemus-prime"
   url "https://github.com/peledies/messagemus-prime/archive/0.0.4.tar.gz"
-  sha256 "c27b790fc83c8f87bc5e393a0396e93b0393c32cdb12b9d4f53996dc92cde4ef"
+  sha256 "edfc8ebc4790f09316b0dc598876df8ba8fbe71df4cd0ebb3ebb5d8c662cf3e8"
 
   def install
     bin.install "messagemus-prime"
