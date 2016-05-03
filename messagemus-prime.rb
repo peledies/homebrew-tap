e022fee35b26a59f505a25fbe4283ba994a0266b
Deac <deac.karns@gmail.com>
2016-05-02 20:01:34 -0400

    updating sha hash

--- messagemus-prime.rb
+++ messagemus-prime.rb
@@ -2,7 +2,7 @@ class MessagemusPrime < Formula
   desc "CLI utility for displaying messages to the top of the terminal"
   homepage "https://github.com/peledies/messagemus-prime"
   url "https://github.com/peledies/messagemus-prime/archive/0.0.3.tar.gz"
-  sha256 "2c34f4cbe78ea1df53817a5fcc805e82668eeed084ddb2aaa1ed2c50b72cc8ca"
+  sha256 "c27b790fc83c8f87bc5e393a0396e93b0393c32cdb12b9d4f53996dc92cde4ef"
 
   def install
     bin.install "messagemus-prime"
