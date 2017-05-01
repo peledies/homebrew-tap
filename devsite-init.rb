eff2592db305acc82b009fb3dc3cb6199ff7478b
Deac Karns <deac@sfp.net>
2017-05-01 17:28:11 -0400

    matching devsite project structure

--- devsite-init.rb
+++ devsite-init.rb
@@ -1,11 +1,11 @@
 class DevsiteInit < Formula
   desc "CLI utility for Initializing a new Develper environment"
   homepage "https://github.com/peledies/devsite-init"
-  url "https://github.com/peledies/devsite-init/archive/0.0.5.tar.gz"
+  url "https://github.com/peledies/devsite-init/archive/0.0.6.tar.gz"
   sha256 "90ffe1db97daa2edde27532a3a6bb1a9d8fa271c8ba1b3e18e236595fbbe2153"
 
   def install
-    bin.install "devsite.sh"
+    bin.install "devsite"
   end
 
   test do
