eaa5187aa1761effa54ddcc59f3af530f5ecb62f
Deac Karns <deac@sfp.net>
2017-05-01 17:16:26 -0400

    add devsite-init repo

---
+++ devsite-init.rb
@@ -0,0 +1,14 @@
+class DevSite < Formula
+  desc "CLI utility for Initializing a new Develper environment"
+  homepage "https://github.com/peledies/devsite-init"
+  url "https://github.com/peledies/devsite-init/archive/master.tar.gz"
+  sha256 "edfc8ebc4790f09316b0dc598876df8ba8fbe71df4cd0ebb3ebb5d8c662cf3e8"
+
+  def install
+    bin.install "devsite"
+  end
+
+  test do
+    system "true"
+  end
+end
