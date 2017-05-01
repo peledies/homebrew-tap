2c7874620f476ef8ab06a0da0756304fd36d6383
Deac Karns <deac@sfp.net>
2017-05-01 17:29:12 -0400

    update sha

--- devsite-init.rb
+++ devsite-init.rb
@@ -2,7 +2,7 @@ class DevsiteInit < Formula
   desc "CLI utility for Initializing a new Develper environment"
   homepage "https://github.com/peledies/devsite-init"
   url "https://github.com/peledies/devsite-init/archive/0.0.6.tar.gz"
-  sha256 "90ffe1db97daa2edde27532a3a6bb1a9d8fa271c8ba1b3e18e236595fbbe2153"
+  sha256 "0059d89246321764a99c645aed5f2012871ace54ae14af2bdfd6f3d03da51a5b"
 
   def install
     bin.install "devsite"
