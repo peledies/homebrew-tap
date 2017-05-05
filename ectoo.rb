6068e3af000c734bb5e6bd568aa3b1782fc8139a
Deac Karns <deac@sfp.net>
2017-05-05 10:46:18 -0400

    update sha for ec2

--- ectoo.rb
+++ ectoo.rb
@@ -1,7 +1,7 @@
 class Ectoo < Formula
   desc "CLI utility for bridging the gap with the AWS CLI"
   homepage "https://github.com/peledies/ectoo"
-  url "https://github.com/peledies/ectoo/archive/0.0.1.tar.gz"
+  url "https://github.com/peledies/ectoo/archive/0.0.2.tar.gz"
   sha256 "80038efb1b80149450936eb05e41f4bb8daf6d621e1a0079e6fd5ebfb22c001e"
 
   def install
