571adbcfcac374bad5e033b987aa906022385b32
Deac Karns <deac@sfp.net>
2017-05-05 10:42:44 -0400

    Add ectoo tap

---
+++ ectoo.rb
@@ -0,0 +1,14 @@
+class EcToo < Formula
+  desc "CLI utility for bridging the gap with the AWS CLI"
+  homepage "https://github.com/peledies/ectoo"
+  url "https://github.com/peledies/ectoo/archive/0.0.1.tar.gz"
+  sha256 "edfc8ebc4790f09316b0dc598876df8ba8fbe71df4cd0ebb3ebb5d8c662cf3e8"
+
+  def install
+    bin.install "ectoo"
+  end
+
+  test do
+    system "true"
+  end
+end
