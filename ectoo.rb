922d7228b31ad6566661b40b99589bf37c16bc2f
Deac Karns <deac@sfp.net>
2017-06-12 13:31:34 -0400

    update version number

--- ectoo.rb
+++ ectoo.rb
@@ -3,7 +3,7 @@
 class Ectoo < Formula
   desc "CLI utility for bridging the gap with the AWS CLI"
   homepage "https://github.com/peledies/ectoo"
-  url "https://github.com/peledies/ectoo/archive/0.0.4.tar.gz"
+  url "https://github.com/peledies/ectoo/archive/0.0.5.tar.gz"
   sha256 "f95ec88f22bd217271199695593f9a217cc0fc3b69469e31cafd09f0a55dc1a4"
 
   def install
